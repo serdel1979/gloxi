97eee4ba6989e3c93d7b7c6651af46e16275ab59
Sergio De Luca <sdlbsso@gmail.com>
2024-10-24 19:11:40 -0300

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,41 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  
+  screenIsLarge = false;
+  isMenuVisible: boolean = false;
+
+  isSearchVisible: boolean = false;
+
   title = 'goxi';
+
+
+  constructor() {
+    this.onResize();
+  }
+
+  // Detectar si el tamaño de la pantalla es grande (md o superior)
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    this.screenIsLarge = window.innerWidth >= 768; // md en Tailwind es 768px
+    if (this.screenIsLarge) {
+      this.isSearchVisible = false; // Resetea la visibilidad del input si la pantalla es grande
+    }
+  }
+
+  // Alternar la visibilidad del input de búsqueda en pantallas pequeñas
+
+
+
+
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  toggleSearch() {
+    this.isSearchVisible = !this.isSearchVisible;
+  }
+
 }
