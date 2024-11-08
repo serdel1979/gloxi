a148281fdb1ee2ac6733d9066a46033ef476c549
sergio <sdlbsso@gmail.com>
2024-11-08 10:50:08 -0300

    lazy load hash url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, HostListener } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
